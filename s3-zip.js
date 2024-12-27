268064b9a3541483f720b9bb936775e763fe1567
ardencod3 <ardencod3@gmail.com>
2024-12-27 08:21:13 +0700

    Update s3-zip.js
    add file list name build

--- s3-zip.js
+++ s3-zip.js
@@ -1,97 +1,74 @@
-const s3Files = require('s3-files')
-const archiver = require('archiver')
+var s3Files = require('s3-files')
+var archiver = require('archiver')
+let path = require('path')
 
-const s3Zip = {}
+var s3Zip = {}
 module.exports = s3Zip
 
 s3Zip.archive = function (opts, folder, filesS3, filesZip) {
-  const self = this
-  let connectionConfig
+  var self = this
+  var keyStream = s3Files
+    .connect({
+      region: opts.region,
+      bucket: opts.bucket
+    })
+    .createKeyStream(folder, filesS3.list)
 
-  this.folder = folder
-
-  self.debug = opts.debug || false
-
-  if ('s3' in opts) {
-    connectionConfig = {
-      s3: opts.s3
-    }
-  } else {
-    connectionConfig = {
-      region: opts.region
-    }
-  }
-
-  connectionConfig.bucket = opts.bucket
-
-  self.client = s3Files.connect(connectionConfig)
-
-  const keyStream = self.client.createKeyStream(folder, filesS3)
-
-  const preserveFolderStructure = opts.preserveFolderStructure === true || filesZip
-  const fileStream = s3Files.createFileStream(keyStream, preserveFolderStructure)
-  const archive = self.archiveStream(fileStream, filesS3, filesZip)
-
+  var fileStream = s3Files.createFileStream(keyStream)
+  var archive = self.archiveStream(fileStream, filesS3, filesZip)
   return archive
 }
 
 s3Zip.archiveStream = function (stream, filesS3, filesZip) {
-  const self = this
-  const folder = this.folder || ''
-  if (this.registerFormat) {
-    archiver.registerFormat(this.registerFormat, this.formatModule)
-  }
-  const archive = archiver(this.format || 'zip', this.archiverOpts || {})
+  let filenameList = createBaseFilenameList(filesS3.list)
+  var archive = archiver('zip')
   archive.on('error', function (err) {
-    self.debug && console.log('archive error', err)
+    console.log('archive error', err)
+    throw err
   })
   stream
     .on('data', function (file) {
+      // console.log(file.data.toString());
       if (file.path[file.path.length - 1] === '/') {
-        self.debug && console.log('don\'t append to zip', file.path)
+        console.log('don\'t append to zip', file.path)
         return
       }
-      let fname
+      var fname
       if (filesZip) {
         // Place files_s3[i] into the archive as files_zip[i]
-        const i = filesS3.indexOf(file.path.startsWith(folder) ? file.path.substr(folder.length) : file.path)
+        var i = filenameList.indexOf(file.path)
         fname = (i >= 0 && i < filesZip.length) ? filesZip[i] : file.path
-        filesS3[i] = ''
       } else {
         // Just use the S3 file name
         fname = file.path
       }
-      const entryData = typeof fname === 'object' ? fname : { name: fname }
-      self.debug && console.log('append to zip', fname)
-      if (file.data.length === 0) {
-        archive.append('', entryData)
-      } else {
-        archive.append(file.data, entryData)
-      }
+      console.log('append to zip', fname)
+      archive.append(file.data, { name: fname })
     })
     .on('end', function () {
-      self.debug && console.log('end -> finalize')
+      console.log('end -> finalize')
       archive.finalize()
     })
-    .on('error', function (err) {
-      archive.emit('error', err)
-    })
+    .on('error', (err) => {
+      try {
+        console.log('don\'t append to zip s3-zip error -> ', err.message);
+        throw err
+      } catch (error) {
+        console.log('catch ', error);
+      }
+      return;
+    });
 
   return archive
 }
 
-s3Zip.setFormat = function (format) {
-  this.format = format
-  return this
-}
+function createBaseFilenameList(filesS3) {
+  // console.log(filesS3)
+  let basefileList = []
+  filesS3.map(function (item) {
+    let filename = path.basename(item)
+    basefileList.push(filename || item)
+  })
 
-s3Zip.setArchiverOptions = function (archiverOpts) {
-  this.archiverOpts = archiverOpts
-  return this
-}
-
-s3Zip.setRegisterFormatOptions = function (registerFormat, formatModule) {
-  this.registerFormat = registerFormat
-  this.formatModule = formatModule
-  return this
+  return basefileList
 }
